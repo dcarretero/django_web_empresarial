91c2331c05ca1ea50160224a5345081c84840bf7
Daniel Carretero <daniel.carretero@gmail.com>
2023-11-07 22:59:02 +0100

    punto 33

--- web_empresarial/urls.py
+++ web_empresarial/urls.py
@@ -20,3 +20,26 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+
+from django.contrib import admin
+from django.urls import path
+from core import views as core_views
+
+
+from django.conf import settings
+
+urlpatterns = [
+    path('',core_views.home,name='home'),
+    path('about/',core_views.about,name='about'),
+    path('contact/',core_views.contact,name='contact'),
+    path('blog/',core_views.blog,name='blog'),
+    path('sample/',core_views.sample,name='sample'),
+    path('services/',core_views.services,name='services'),
+    path('store/',core_views.store,name='store'),     
+    path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
